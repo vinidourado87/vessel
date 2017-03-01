7a5a97d8169b41ea0493296ee747a2ba109ae991
Vinicius Dourado <vinidourado87@gmail.com>
2017-03-01 01:41:57 -0300

    Improve test

--- app/models/Vessel.scala
+++ app/models/Vessel.scala
@@ -12,21 +12,21 @@ object Vessel {
     Vessel(name = name, width = width, length = length, draft = draft, latitude = latitude, longitude = longitude)
 
   implicit val reads: Reads[Vessel] = (
-  (__ \ "name").read[String] and
-  (__ \ "width").read[Double] and
-  (__ \ "length").read[Double] and
-  (__ \ "draft").read[Double] and
-  (__ \ "latitude").read[String] and
-  (__ \ "longitude").read[String]
+    (__ \ "name").read[String] and
+    (__ \ "width").read[Double] and
+    (__ \ "length").read[Double] and
+    (__ \ "draft").read[Double] and
+    (__ \ "latitude").read[String] and
+    (__ \ "longitude").read[String]
    )(makeVessel _)
    
    implicit val writes: Writes[Vessel] = (
-  (__ \ "id").write[Long] and
-  (__ \ "name").write[String] and
-  (__ \ "width").write[Double] and
-  (__ \ "length").write[Double] and
-  (__ \ "draft").write[Double] and
-  (__ \ "latitude").write[String] and
-  (__ \ "longitude").write[String]
+    (__ \ "id").write[Long] and
+    (__ \ "name").write[String] and
+    (__ \ "width").write[Double] and
+    (__ \ "length").write[Double] and
+    (__ \ "draft").write[Double] and
+    (__ \ "latitude").write[String] and
+    (__ \ "longitude").write[String]
    )(unlift(Vessel.unapply))
 }
