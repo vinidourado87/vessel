f6d67b25cecc1a0b326651082f90e839c7c659fa
Vinicius Dourado <vinidourado87@gmail.com>
2017-02-28 21:03:57 -0300

    Start to create vessel models and mappings

---
+++ app/dao/VesselRepository.scala
@@ -0,0 +1,44 @@
+package dao
+
+import javax.inject.{ Inject, Singleton }
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+
+import models.Vessel
+
+import scala.concurrent.{ Future, ExecutionContext }
+
+@Singleton
+class VesselRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+  
+  import dbConfig._
+  import driver.api._
+  
+  private class VesselTable(tag: Tag) extends Table[Vessel](tag, "vessel") {
+
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+    def width = column[Double]("width")
+    def length = column[Double]("length")
+    def draft = column[Double]("draft")
+    def latitude = column[String]("latitude")
+    def longitude = column[String]("longitude")
+
+    def * = (id, name, width, length, draft, latitude, longitude) <> ((Vessel.apply _).tupled, Vessel.unapply)
+  }
+  
+  private val vessel = TableQuery[VesselTable]
+
+  def create(name: String, width: Double, length: Double, draft: Double, latitude: String, longitude: String): Future[Vessel] = db.run {
+    (vessel.map(p => (p.name, p.width, p.length, p.draft, p.latitude, p.longitude))
+      returning vessel.map(_.id)
+      into ((vessel, id) => Vessel(id, vessel._1, vessel._2, vessel._3, vessel._4, vessel._5, vessel._6))
+    ) += (name, width, length, draft, latitude, longitude)
+  }
+
+  def list(): Future[Seq[Vessel]] = db.run {
+    vessel.result
+  }
+}
---
+++ app/models/Vessel.scala
@@ -0,0 +1,32 @@
+package models
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class Vessel(id: Long = 0, name: String, width: Double, length: Double, draft: Double,
+    latitude: String, longitude: String)
+
+object Vessel {
+
+  def makeVessel(name: String, width: Double, length: Double, draft: Double, latitude: String, longitude: String) = 
+    Vessel(name = name, width = width, length = length, draft = draft, latitude = latitude, longitude = longitude)
+
+  implicit val reads: Reads[Vessel] = (
+  (__ \ "name").read[String] and
+  (__ \ "width").read[Double] and
+  (__ \ "length").read[Double] and
+  (__ \ "draft").read[Double] and
+  (__ \ "latitude").read[String] and
+  (__ \ "longitude").read[String]
+   )(makeVessel _)
+   
+   implicit val writes: Writes[Vessel] = (
+  (__ \ "id").write[Long] and
+  (__ \ "name").write[String] and
+  (__ \ "width").write[Double] and
+  (__ \ "length").write[Double] and
+  (__ \ "draft").write[Double] and
+  (__ \ "latitude").write[String] and
+  (__ \ "longitude").write[String]
+   )(unlift(Vessel.unapply))
+}
