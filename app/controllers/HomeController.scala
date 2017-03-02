ecfded769b6813a86a9c7ed63592b1a5f2c2f209
Vinicius Dourado <vinidourado87@gmail.com>
2017-03-02 08:02:25 -0300

    Split controller

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject._
+
+import dao._
+import models.Vessel
+import play.api.i18n._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
+
+class HomeController @Inject()(repo: VesselRepository, val messagesApi: MessagesApi)
+                                (implicit ec: ExecutionContext) extends Controller with I18nSupport {
+  
+  def index() = Action {
+    Redirect("/index.html")
+  }
+}
