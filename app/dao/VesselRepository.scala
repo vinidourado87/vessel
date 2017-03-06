1d1e9a48808e1a998082aa4ec9452f2ed0613259
Vinicius Dourado <vinidourado87@gmail.com>
2017-03-05 23:05:33 -0300

    Rename variable

--- app/dao/VesselRepository.scala
+++ app/dao/VesselRepository.scala
@@ -13,10 +13,10 @@ import java.sql.Date
 class VesselRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
 
   private val dbConfig = dbConfigProvider.get[JdbcProfile]
-  
+
   import dbConfig._
   import driver.api._
-  
+
   private class VesselTable(tag: Tag) extends Table[Vessel](tag, "vessel") {
 
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
@@ -30,31 +30,31 @@ class VesselRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impl
 
     def * = (id, name, width, length, draft, latitude, longitude, updatedAt) <> ((Vessel.apply _).tupled, Vessel.unapply)
   }
-  
-  private val vessel = TableQuery[VesselTable]
+
+  private val vesselTable = TableQuery[VesselTable]
 
   def createOrUpdate(vessel: Vessel) {
-    val query = this.vessel.insertOrUpdate(vessel)
+    val query = this.vesselTable.insertOrUpdate(vessel)
     db.run(query)
   }
 
   def list(): Future[Seq[Vessel]] = db.run {
-    vessel.result
+    vesselTable.result
   }
 
   def relocate(id: Long, newLatitude: Double, newLongitude: Double) = {
-    val query = vessel.filter(_.id === id).map(v => (v.latitude, v.longitude)).update((newLatitude, newLongitude))
+    val query = vesselTable.filter(_.id === id).map(v => (v.latitude, v.longitude)).update((newLatitude, newLongitude))
     db.run(query)
     find(id)
   }
 
   def find(id: Long) = {
-    val query = vessel.filter(_.id === id).result.headOption
+    val query = vesselTable.filter(_.id === id).result.headOption
     db.run(query)
   }
 
   def delete(id: Long) {
-    val query = vessel.filter(_.id === id)
+    val query = vesselTable.filter(_.id === id)
     db.run(query.delete)
   }
 }
